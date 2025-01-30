fba6dc473b712b25b24a8070a9933d85e5a2cae2
Work <holiak.m.v@gmail.com>
2025-01-30 21:49:02 +0200

    fix adaptive

--- src/components/Modules.tsx
+++ src/components/Modules.tsx
@@ -12,34 +12,36 @@ interface ModulesProps {
 
 export default function Modules({ cards }: ModulesProps) {
 	return (
-		<div className='grid 2xs:mx-auto 2xs:grid-cols-[1fr] 2xs:max-w-[640px] lg:max-w-[none] lg:grid-cols-[1fr_1fr] gap-[28px]'>
+		<div className='grid 2xs:mx-auto 2xs:grid-cols-[1fr] 2xs:max-w-[500px] lg:max-w-[1028px] lg:grid-cols-[1fr_1fr] gap-[28px]'>
 			{cards.map((card, index) => (
 				<div
 					className='p-[32px] bg-gradient-to-t from-[#D2E4ED] to-[transparent] border-[1px] border-[#247CA8] rounded-[32px]'
 					key={index}
 				>
-					<div className='flex justify-between items-center 2xs:mb-[32px] lg:mb-[70px]'>
+					<div className='2xs:mb-[32px] lg:mb-[70px]'>
 						<div>
 							<i>
-								<h3 className='font-semibold'>
-									<span className='text-[100px]'>{card.number}</span> модуль
+								<h3 className='font-semibold flex items-center justify-between'>
+									<p>
+										<span className='text-[100px]'>{card.number}</span> модуль
+									</p>
+									<Image
+										src={'/images/assets-15.png'}
+										width={42}
+										height={42}
+										alt='star'
+										className=''
+									/>
 								</h3>
 							</i>
 							<h3 className='text-[#247CA8]'>{card.title}</h3>
 						</div>
-
-						<Image
-							src={'/images/assets-15.png'}
-							width={42}
-							height={42}
-							alt='star'
-						/>
 					</div>
 
 					<ul>
 						{card.list.map((listItem, listIndex) => (
 							<div key={listIndex} className='flex gap-4 items-center mb-2'>
-								<span className='w-[12px] h-[12px] rounded-full bg-[#247CA8]'></span>
+								<span className='min-w-[12px] h-[12px] rounded-full bg-[#247CA8]'></span>
 								<li className='font-light'>{listItem}</li>
 							</div>
 						))}
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { AudioPlayer } from './index'
 
 interface ReviewItem {
@@ -16,12 +17,7 @@ const StarRating = ({ rating }: { rating: number }) => {
 	return (
 		<div className='flex gap-1'>
 			{stars.map((filled, index) => (
-				<span
-					key={index}
-					className={`${filled ? 'text-[#ffe73e]' : 'text-gray-400'} sm:text-[32px] sm:leading-[32px]`}
-				>
-					★
-				</span>
+				<Image key={index} src={`/images/${filled ? 'star' : 'star_grey'}.svg`} width={20} height={20} className='sm:w-[32px] sm:h-[32px]' alt='star' />
 			))}
 		</div>
 	)
@@ -29,9 +25,9 @@ const StarRating = ({ rating }: { rating: number }) => {
 
 export default function Reviews({ reviews }: ReviewsProps) {
 	return (
-		<div className='grid 2xs:mx-auto 2xs:max-w-[630px] lg:mx-0 lg:max-w-[none] lg:grid-cols-[1fr_1fr] gap-[28px]'>
+		<div className='grid 2xs:mx-auto 2xs:max-w-[630px] lg:mx-0 lg:max-w-[none] lg:grid-cols-[1fr_1fr] 2xs:gap-[14px] lg:gap-[28px]'>
 			{reviews.map((review, index) => (
-				<div key={index} className='p-[30px_36px] bg-white rounded-[32px]'>
+				<div key={index} className='p-[30px_36px] bg-white rounded-[20px]'>
 					<div className='flex flex-col items-end gap-2 mb-[28px]'>
 						<StarRating rating={review.rating} />
 						<p className='mr-4 sm:text-[30px] sm:leading-[36px]'>{review.name}</p>
